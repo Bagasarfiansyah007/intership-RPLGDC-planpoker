2349fde8a37955b78ec049559cdfdfeb86628536
raflyfarhandika <raflyfarhandika@gmail.com>
2021-08-08 17:21:27 +0700

    init2
    CRUD masih error

--- RoomModel.php
+++ RoomModel.php
@@ -7,7 +7,7 @@ class Room {
     private $namaRoom;
     private $time;
 
-    public function __construct($roomCode, $idRoom, $namaRoom, $time){
+    public function __construct($idRoom, $roomCode, $namaRoom, $time){
         $this->roomCode = $roomCode;
         $this->idRoom = $idRoom;
         $this->namaRoom = $namaRoom;
@@ -44,6 +44,63 @@ class Room {
 
     function get_time(){
         return $this->time;
+    }
+
+    function add_room(){
+        $roomCode = $_POST['roomCode'];
+		$namaRoom = $_POST['namaRoom'];
+		$time = $_POST['time'];
+		
+		// include database connection file
+		include_once("config.php");
+				
+		// Insert user data into table
+		$result = mysqli_query($mysqli, "INSERT INTO room(roomCode,namaRoom,time) VALUES('$roomCode','$namaRoom','$time')");
+		
+		// Show message when user added
+		// echo "User added successfully. <a href='index.php'>View Users</a>";
+    }
+
+    function edit_room(){
+        // include database connection file
+        include_once("config.php");
+
+        $idRoom = $_POST['idRoom'];
+	
+	    $roomCode = $_POST['roomCode'];
+		$namaRoom = $_POST['namaRoom'];
+		$time = $_POST['time'];
+		
+        // update user data
+        $result = mysqli_query($mysqli, "UPDATE room SET roomCode='$roomCode',namaRoom='$namaRoom',time='$time' WHERE idRoom=$idRoom");
+        
+        // Redirect to homepage to display updated user in list
+        // header("Location: index.php");
+
+        // Display selected user data based on id
+        // Getting id from url
+        $idRoom = $_GET['idRoom'];
+        
+        // Fetech user data based on id
+        $result = mysqli_query($mysqli, "SELECT * FROM room WHERE idRoom=$idRoom");
+        
+        while($user_data = mysqli_fetch_array($result))
+        {
+            $roomCode = $user_data['roomCode'];
+            $namaRoom = $user_data['namaRoom'];
+            $time = $user_data['time'];
+        }
+    }
+
+    function delete_room(){
+        // include database connection file
+        include_once("config.php");
+ 
+        // Get id from URL to delete that user
+        $idRoom = $_GET['idRoom'];
+        
+        // Delete user row from table based on given id
+        $result = mysqli_query($mysqli, "DELETE FROM room WHERE idRoom=$idRoom");
     }
 }
 
--- StoriesModel.php
+++ StoriesModel.php
@@ -7,7 +7,7 @@ class Stories {
     private $story;
     private $isDone;
 
-    public function __construct($idStories, $overallValue, $story, $isDone){
+    public function __construct($idStories , $overallValue, $story, $isDone){
         $this->idStories = $idStories;
         $this->overallValue = $overallValue;
         $this->story = $story;
@@ -44,6 +44,63 @@ class Stories {
 
     function get_isDone(){
         return $this->isDone;
+    }
+
+    function add_story(){
+        $story = $_POST['story'];
+		$overallValue = $_POST['overallValue'];
+		$isDone = $_POST['isDone'];
+		
+		// include database connection file
+		include_once("config.php");
+				
+		// Insert user data into table
+		$result = mysqli_query($mysqli, "INSERT INTO stories(story, overallValue, isDone) VALUES('$story','$overallValue','$isDone')");
+		
+		// Show message when user added
+		// echo "User added successfully. <a href='index.php'>View Users</a>";
+    }
+
+    function edit_room(){
+        // include database connection file
+        include_once("config.php");
+
+        $idStory = $_POST['idStory'];
+	
+	    $story = $_POST['story'];
+		$overallValue = $_POST['overallValue'];
+		$isDone = $_POST['isDone'];
+		
+        // update user data
+        $result = mysqli_query($mysqli, "UPDATE stories SET story='$story',overallValue='$overallValue',isDone='$isDone' WHERE idStories=$idStories");
+        
+        // Redirect to homepage to display updated user in list
+        // header("Location: index.php");
+
+        // Display selected user data based on id
+        // Getting id from url
+        $idStories = $_GET['idStories'];
+        
+        // Fetech user data based on id
+        $result = mysqli_query($mysqli, "SELECT * FROM stories WHERE idStories=$idStories");
+        
+        while($user_data = mysqli_fetch_array($result))
+        {
+            $story = $user_data['story'];
+            $overallValue = $user_data['overallValue'];
+            $isDone = $user_data['isDone'];
+        }
+    }
+
+    function delete_room(){
+        // include database connection file
+        include_once("config.php");
+ 
+        // Get id from URL to delete that user
+        $idStories = $_GET['idStories'];
+        
+        // Delete user row from table based on given id
+        $result = mysqli_query($mysqli, "DELETE FROM stories WHERE idStories=$idStories");
     }
 }
 
